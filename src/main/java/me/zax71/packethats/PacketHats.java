e95352f02b00038d12b74375ce1346b631969aaf
Zax71 <zabax32@gmail.com>
2022-03-26 19:08:14 +0000

    Hello World

---
+++ src/main/java/me/zax71/packethats/PacketHats.java
@@ -0,0 +1,17 @@
+package me.zax71.packethats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PacketHats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
